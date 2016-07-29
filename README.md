d1274edb3d1f7eb40a3b53f51f4f4ddd6ed9bbb5
darylbehrens <drb@precisiontv.com>
2016-07-29 12:18:10 -0700

    Made Program PEP8 compliant and updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# UdacityEntertainmentCenter
+Udacity Entertainment Center
+
+Creator
+-----------------------------------
+Daryl Behrens - darylbehrens@gmail.com
+
+
+Latest Build Date
+-----------------------------------
+7/29/2016
+
+
+What it does?
+-----------------------------------
+Creates a webpage showing movies posters and movie meta data
+allowing user to click on movie and watch trailer
+
+
+Installation
+-----------------------------------
+Use python to run entertainment_center.py
+
+
+Dependencies
+-----------------------------------
+media.pyc 
+	Movie class allows you to enter Movie metadata
+	and show the trailer
+
+entertainment_center.py
+	This is the main program, where you can create instances
+	of a movie, and create the website
+
+fresh_tomatoes.py
+	This is an Udacity supplied library that creates the 
+	HTML, CSS, and javascript for the web page.
--- entertainment_center.py
+++ entertainment_center.py
@@ -4,41 +4,49 @@ import fresh_tomatoes
 # Create Movie instances
 toy_story = media.Movie("Toy Story",
                         "A Story of a boy and his toys",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/"
+                        "Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4",
-						"G")
+                        "G")
 
 avatar = media.Movie("Avatar",
-                    "A Marine on an alien planet",
-                    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                    "https://www.youtube.com/watch?v=5PSNL1qE6VY",
-					"PG-13")
-                    
+                     "A Marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/"
+                     "en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY",
+                     "PG-13")
+
 magnolia = media.Movie("Magnolia",
                        "Fucking Crazy Movie",
-                       "https://upload.wikimedia.org/wikipedia/en/4/4b/Magnolia_poster.png",
+                       "https://upload.wikimedia.org/wikipedia/en"
+                       "/4/4b/Magnolia_poster.png",
                        "www.youtube.com/watch?v=cxcegktcxSM",
-					   "NC-17")
+                       "NC-17")
 
 eternalSunshine = media.Movie("Eternal Sunshine of the Spotless Mind",
-                              "A man and women fall back in love, after forgetting their past relationship",
-                              "https://upload.wikimedia.org/wikipedia/en/6/62/Eternal_sunshine_of_the_spotless_mind_ver3.jpg",
+                              "A man and women fall back in love, after "
+                              "forgetting their past relationship",
+                              "https://upload.wikimedia.org/wikipedia/en/6/62/"
+                              "Eternal_sunshine_of_the_spotless_mind_ver3.jpg",
                               "https://www.youtube.com/watch?v=rb9a00bXf-U",
                               "R")
 
 machinist = media.Movie("The Machinist",
                         "A man slowly loses his mind due to insomnia",
-                        "https://upload.wikimedia.org/wikipedia/en/b/b9/The_Machinist_poster.JPG",
+                        "https://upload.wikimedia.org/wikipedia/en/b/"
+                        "b9/The_Machinist_poster.JPG",
                         "https://www.youtube.com/watch?v=H0fuHY4U1UA",
                         "R")
 
 memento = media.Movie("Memento",
-                      "A man wakes up each day with no memory, and tries to solve a mystery",
-                      "https://upload.wikimedia.org/wikipedia/en/c/c7/Memento_poster.jpg",
+                      "A man wakes up each day with no memory"
+                      ", and tries to solve a mystery",
+                      "https://upload.wikimedia.org/wikipedia/en"
+                      "/c/c7/Memento_poster.jpg",
                       "https://www.youtube.com/watch?v=0vS0E9bBSL0",
                       "R")
 
-#Add Movies To A List
+# Add Movies To A List
 movies = [toy_story, avatar, magnolia, eternalSunshine, machinist, memento]
 
 # using provided class, create HTML page with movies
